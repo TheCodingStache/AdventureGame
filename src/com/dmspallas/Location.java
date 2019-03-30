bc004ad28a14e7b3a2bc98c2614b2a3f1ed5088f
dmspallas <dmspallas@gmail.com>
2019-03-30 04:15:58 +0200

    The classes are now immutable. This is a defensive technique to avoid 3rd party actions that can change our code. For instance: The map now is initialized into the constructor which allows us to give parameters which cannot be changed from main class. Also the map which is created into that constructor is passed with final fields(like constants). In the getter (getExits) method we return a new replica of our map which gives us the advantage to interact with a copy of the initial map and not with the "official" data that have been given into the object.

--- src/com/dmspallas/Location.java
+++ src/com/dmspallas/Location.java
@@ -8,13 +8,20 @@ public class Location {
     private final String description;
     private final Map<String, Integer> exits;
 
+    public Location(int locationID, String description, Map<String, Integer> exits) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exits = new HashMap<>(exits);
+        this.exits.put("Q", 0);
+    }
+
     public int getLocationID() {
         return locationID;
     }
 
-    public void addExit(String direction, int location) {
-        exits.put(direction, location);
-    }
+//    public void addExit(String direction, int location) {
+//        exits.put(direction, location);
+//    }
 
     public String getDescription() {
         return description;
@@ -23,12 +30,5 @@ public class Location {
     public Map<String, Integer> getExits() {
         return new HashMap<String, Integer>(exits) {
         };
-    }
-
-    public Location(int locationID, String description) {
-        this.locationID = locationID;
-        this.description = description;
-        this.exits = new HashMap<>();
-        this.exits.put("Q", 0);
     }
 }
